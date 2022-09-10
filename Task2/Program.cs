06c6738016b3e5f19ad0e83eb8d248d24bb6867a
Olga <politykoolga@outlook.com>
2022-09-10 21:09:48 +0300

    Second commit

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int tmp = num;
+int sum = 0;
+
+if (num > 0)
+    {
+        while (tmp != 0)
+        {
+            if (num % (tmp%10) ==0)
+            sum += tmp%10;
+
+            tmp = tmp / 10;
+        }
+        Console.WriteLine(sum);
+    }
+             
