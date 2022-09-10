9b36f71771b1fb2c22169b898f66a0cddde2791f
Olga <politykoolga@outlook.com>
2022-09-10 21:06:48 +0300

    First commit

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение степени (число B), в которую будет возводиться число А: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int power = A;
+
+for (int i = 1; i < B; i++)
+{
+    power = power * A;
+}
+
+Console.WriteLine("A в степени B равно " + power);
